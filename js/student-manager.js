c364f5d9edab4bfd307f74902e77776447bf58d1
leolfg <leolaffargue1@gmail.com>
2020-04-24 16:30:10 +0200

    getting a nice display maybe ?

--- js/student-manager.js
+++ js/student-manager.js
@@ -15,7 +15,7 @@ AFRAME.registerComponent('student-manager',
 
         var score_text = document.getElementById("score_text")
         var score_text_value = score_text.getAttribute('value');
-        score_text.setAttribute("value", (score_text_value -1))
+        score_text.setAttribute("value", (score_text_value -1)("/10 etudiants d'Estienne"))
         console.log('shit got -1');
     }
 
