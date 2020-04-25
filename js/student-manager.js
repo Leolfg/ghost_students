e151a9d525be71d27e26d6e032cd14787fc17bb1
leolfg <leolaffargue1@gmail.com>
2020-04-25 11:43:59 +0200

    Update student-manager.js

--- js/student-manager.js
+++ js/student-manager.js
@@ -13,11 +13,11 @@ AFRAME.registerComponent('student-manager',
         var score_text = document.getElementById("score_text")
         var score_text_value = score_text.getAttribute('value');
 
-        if (score_text_value > -1){
+        if (score_text_value > 0){
         score_text.setAttribute("value", (score_text_value -5))
         }
 
-        if(score_text_value = 0){
+        if(score_text_value <= 0){
             document.getElementById("camera").setAttribute("position", "10 10 10");
             console.log("the score is 0, player tped")
             //take player to a position
