1c2a872339d1291c9e523d983be2b8f7ab6510db
leolfg <leolaffargue1@gmail.com>
2020-04-25 11:35:38 +0200

    deleting score-init

--- js/score-init.js
+++
@@ -1,9 +0,0 @@
-// JavaScript source code
-AFRAME.registerComponent('score-init',
-{
-    init: function( )
-    {
-        var score_text = this.el;
-        score_text.setAttribute("value", 10)    
-    }
-});
