3824b03cf7a1cb331aded0166fd890fae1d1bbc3
leolfg <leolaffargue1@gmail.com>
2020-04-25 11:52:58 +0200

    two fonctions; calling score_check

--- js/student-manager.js
+++ js/student-manager.js
@@ -4,23 +4,28 @@ AFRAME.registerComponent('student-manager',
     init: function( )
     {
         studenttrigger = this.el
-        studenttrigger.addEventListener('click', function( evt ){test_click()})
+        studenttrigger.addEventListener('click', function( evt ){decrease_score(),check_score()})
     }
     });
 
-    function test_click(){
+    function decrease_score(){
 
         var score_text = document.getElementById("score_text")
         var score_text_value = score_text.getAttribute('value');
 
         if (score_text_value > 0){
-            score_text.setAttribute("value", (score_text_value -1))
+            score_text.setAttribute("value", (score_text_value -5))
         }
+    }
+
         
-        else {
+    function check_score(){
+        if  (score_text_value = 0){
             document.getElementById("camera").setAttribute("position", "10 10 10");
             console.log("the score is 0, player tped")
                         //take player to a position
             //make camille scalabre appear + animation
             //petit texte "Tu as trouvé tous les étudiants de l'école Estienne"
-}
+        }
+
+    }
