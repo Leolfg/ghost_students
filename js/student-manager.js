9acf87ae02654323108060db6fa5c88269a39f8d
leolfg <leolaffargue1@gmail.com>
2020-04-25 12:33:13 +0200

    adding second camera for ending

--- js/student-manager.js
+++ js/student-manager.js
@@ -22,15 +22,17 @@ AFRAME.registerComponent('student-manager',
     function check_score(){
         var score_text = document.getElementById("score_text")
         var score_text_value = score_text.getAttribute('value');
+        var camera = document.getElementById("camera")
+        var camera_2 = document.getElementById("camera_2")
 
         if  (score_text_value == 0){
             score_text.setAttribute("visible", "false");
             document.getElementById("etudiants_text").setAttribute("visible", "false");
-            document.getElementById("camera_rig").setAttribute("rotation","-25 -175 0");
-            document.getElementById("camera").setAttribute("position", "6 8 30");
 
-            console.log("the score is 0, player tped")
-                        //take player to a position
+            camera_2.setAttribute('camera', 'active', true)
+            camera.setAttribute('camera', 'active', false)
+            
+            //take player to a position
             //make camille scalabre appear + animation
             //petit texte "Tu as trouvé tous les étudiants de l'école Estienne"
         }
