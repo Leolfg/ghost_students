1c462b75ff3ba23cc53707858648d10a561e6831
leolfg <leolaffargue1@gmail.com>
2020-04-26 15:29:04 +0200

    jsssssss

--- js/student-manager.js
+++ js/student-manager.js
@@ -5,34 +5,40 @@ AFRAME.registerComponent('student-manager',
     {
         studenttrigger = this.el
         studenttrigger.addEventListener('click', function( evt ){decrease_score(),check_score()})
+
+        
+        function decrease_score(){
+
+            var score_text = document.getElementById("score_text")
+            var score_text_value = score_text.getAttribute('value');
+    
+            if (score_text_value > 0){
+                score_text.setAttribute("value", (score_text_value -1))
+            }
+        }
+
+
+        function check_score(){
+            var score_text = document.getElementById("score_text")
+            var score_text_value = score_text.getAttribute('value');
+            var camera_2 = document.getElementById("camera_2")
+    
+            var camille = document.getElementById("camille_end")
+    
+            if  (score_text_value == 0){
+                score_text.setAttribute("visible", "false");
+                document.getElementById("etudiants_text").setAttribute("visible", "false");
+                
+                camille.emit('theend');
+    
+                camera_2.setAttribute('camera', 'active', true)
+            }
+    
+        }
+
     }
     });
 
-    function decrease_score(){
 
-        var score_text = document.getElementById("score_text")
-        var score_text_value = score_text.getAttribute('value');
-
-        if (score_text_value > 0){
-            score_text.setAttribute("value", (score_text_value -1))
-        }
-    }
 
         
-    function check_score(){
-        var score_text = document.getElementById("score_text")
-        var score_text_value = score_text.getAttribute('value');
-        var camera_2 = document.getElementById("camera_2")
-
-        var camille = document.getElementById("camille_end")
-
-        if  (score_text_value == 0){
-            score_text.setAttribute("visible", "false");
-            document.getElementById("etudiants_text").setAttribute("visible", "false");
-            
-            camille.emit('theend');
-
-            camera_2.setAttribute('camera', 'active', true)
-        }
-
-    }
