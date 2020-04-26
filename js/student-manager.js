5ff4fed46ff6c6b19e95072452d0ee9aa527f35e
leolfg <leolaffargue1@gmail.com>
2020-04-26 16:43:54 +0200

    Update student-manager.js

--- js/student-manager.js
+++ js/student-manager.js
@@ -5,14 +5,13 @@ AFRAME.registerComponent('student-manager',
     {   
         var studenttrigger = this.el
         var audio = this.el.components.sound;
+        
         //If sound is attached to the student directly lmao
         /*console.log("this studenttrigger" +studenttrigger.id)
         var audio_name = studenttrigger.id.replace('trigger_','audio_');
         var audio_file = document.querySelector('#'+ audio_name);
         console.log("audio is "+ audio_file.id);*/
-        this.el.addEventListener('click', () => {
-            audio.play
-       });
+        this.el.addEventListener('click', () => {audio.playSound()});
 
         //studenttrigger.addEventListener('click', function( evt ){decrease_score(),check_score(), play_sound()})
 
