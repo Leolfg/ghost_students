48226d403639d697b34897f0cbd8e2a7f0571977
leolfg <leolaffargue1@gmail.com>
2020-04-26 15:05:53 +0200

    model opacity for everyone + camille + event js

--- js/student-manager.js
+++ js/student-manager.js
@@ -24,9 +24,13 @@ AFRAME.registerComponent('student-manager',
         var score_text_value = score_text.getAttribute('value');
         var camera_2 = document.getElementById("camera_2")
 
+        var camille = document.getElementById("camille_end")
+
         if  (score_text_value == 0){
             score_text.setAttribute("visible", "false");
             document.getElementById("etudiants_text").setAttribute("visible", "false");
+            
+            camille.emit('theend');
 
             camera_2.setAttribute('camera', 'active', true)
         }
